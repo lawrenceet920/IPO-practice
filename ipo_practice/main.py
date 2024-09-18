e5493937e459b278f4c266441d8f384282a2e06b
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-09-18 14:04:23 -0400

    Add files via upload

---
+++ ipo_practice/main.py
@@ -0,0 +1,16 @@
+# Ethan Lawrence
+# IP Intro
+# Spet 9 2025
+
+# input
+num1 = int(input('Enter a number: (Example:9)\n'))
+num2 = int(input('Enter another number: (Example:43)\n'))
+
+# process 
+total = num1 + num2
+
+# Output
+print(f'First number entered was: {num1}')
+print(f'Second number entered was: {num2}')
+print()
+print(f'The sum of {num1} and {num2} is: {total}')
